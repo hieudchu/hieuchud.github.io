f28e3870c984b86f8d042928be84075923649404
seta-tanpham <tan.pham@setacinq.vn>
2023-10-21 14:28:44 +0700

    add animated boxes

---
+++ script.js
@@ -0,0 +1,7 @@
+function navigateToPage(pageNumber) {
+    if (pageNumber === 1) {
+        window.location.href = 'page1.html';
+    } else if (pageNumber === 2) {
+        window.location.href = 'page2.html';
+    }
+}
