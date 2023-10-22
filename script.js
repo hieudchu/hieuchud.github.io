d1ed61db875f9694b0bb805195a2c37c5fabd590
seta-tanpham <tan.pham@setacinq.vn>
2023-10-22 14:51:03 +0700

    Fix iphone 14 pro max screen size and onClick animation

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function navigateToPage(pageNumber) {
         window.location.href = 'page2.html';
     }
 }
+
+
+function addClickAnimation(box) {
+    box.classList.add('onClickAnimation'); // Add the onClick animation class
+
+    setTimeout(function () {
+        box.classList.remove('onClickAnimation'); // Remove the animation class
+    }, 1000); // Adjust the time (in milliseconds) to match the duration of your onClick animation
+}
